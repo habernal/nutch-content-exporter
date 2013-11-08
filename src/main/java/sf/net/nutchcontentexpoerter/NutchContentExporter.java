8e9c1c006eb402d72918664279ed06385041c4b8
USER-UKP <user-ukp@DIPF-UKP-IH-NB1.(none)>
2013-11-08 08:21:58 +0100

    Initial commit

---
+++ src/main/java/sf/net/nutchcontentexpoerter/NutchContentExporter.java
@@ -0,0 +1,57 @@
+package sf.net.nutchcontentexpoerter;
+
+import org.apache.nutch.util.NutchConfiguration;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+
+/**
+ *
+ */
+public class NutchContentExporter {
+/*
+    public static void main(String[] args) {
+
+        if (args.length != 2) {
+            System.out.println("usage: segmentdir (-local | -dfs <namenode:port>) outputdir");
+            return;
+        }
+
+        try {
+            Configuration conf = NutchConfiguration.create();
+            FileSystem fs = FileSystem.get(conf);
+
+
+            String segment = args[0];
+
+            File outDir = new File(args[1]);
+            if (!outDir.exists()) {
+                if (outDir.mkdir()) {
+                    System.out.println("Creating output dir " + outDir.getAbsolutePath());
+                }
+            }
+
+            Path file = new Path(segment, Content.DIR_NAME + "/part-00000/data");
+            SequenceFile.Reader reader = new SequenceFile.Reader(fs, file, conf);
+
+
+            Text key = new Text();
+            Content content = new Content();
+
+            while (reader.next(key, content)) {
+                String filename = key.toString().replaceFirst("http://", "").replaceAll("/", "___").trim();
+
+                File f = new File(outDir.getCanonicalPath() + "/" + filename);
+                FileOutputStream fos = new FileOutputStream(f);
+                fos.write(content.getContent());
+                fos.close();
+                System.out.println(f.getAbsolutePath());
+            }
+            reader.close();
+            fs.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+*/
+}
